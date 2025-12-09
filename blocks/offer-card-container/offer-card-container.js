cacfbf12cade40eef1379237eef89d024946c68d
mathai <mathai@adobe.com>
2025-12-09 16:19:22 +0800

    Updates to offer card container block

--- blocks/offer-card-container/offer-card-container.js
+++ blocks/offer-card-container/offer-card-container.js
@@ -1,4 +1,21 @@
 export default function decorate(block) {
-    block.classList.add('offer-card-container__wrapper');
-    console.log("offer card container block", block);
+    const [offerCardContainerWrapper] = block.children;
+
+    const offerCardHTML = document.createElement('div');
+    offerCardHTML.classList.add('offer-card-container__wrapper');
+
+    const children = Array.from(offerCardContainerWrapper.children);
+    children.forEach(child => {
+        if (child.tagName === 'P') {
+            const title = document.createElement('h3');
+            title.textContent = child.textContent.trim();
+            offerCardHTML.appendChild(title);
+        }
+    });
+
+    const offerCardHolder = document.createElement('div');
+    offerCardHolder.classList.add('offer-card-container__holder');
+    offerCardHTML.appendChild(offerCardHolder);
+
+    offerCardContainerWrapper.replaceChildren(offerCardHTML);
   }
--- blocks/offer-card/offer-card.js
+++ blocks/offer-card/offer-card.js
@@ -1,4 +1,3 @@
 export default function decorate(block) {
-  block.classList.add('offer-card--main-wrapper');
-  console.log("offer card block", block);
+  
 }
